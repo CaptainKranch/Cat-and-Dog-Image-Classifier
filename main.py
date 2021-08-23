1cb2dbe62aa8429074a86a8facdf1ef2b22cd329
Daniel Garcia M <daniel.garcia9@udea.edu.co>
2021-08-23 10:05:39 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
