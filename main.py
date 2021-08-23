f3efa6581e9e49674dd42d085380827ef529c432
Daniel Garcia M <daniel.garcia9@udea.edu.co>
2021-08-23 10:15:13 -0500

    ud

--- main.py
+++ main.py
@@ -6,3 +6,20 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+
+PATH = 'cats_and_dogs'
+train_dir = os.path.join(PATH, 'train')
+validation_dir = os.path.join(PATH, 'validation')
+test_dir = os.path.join(PATH, 'test')
+
+total_train = sum([len(files) for r, d, files in os.walk(train_dir)])
+print(total_train)
+total_val = sum([len(files) for r, d, files in os.walk(validation_dir)])
+print(total_val)
+# total_test = len(os.listdir(test_dir))
+
+# Variables for pre-processing and training.
+batch_size = 128
+epochs = 15
+IMG_HEIGHT = 150
+IMG_WIDTH = 150
